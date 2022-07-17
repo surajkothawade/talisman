c009ffe52c6ad3b271b7f2e639adef37924bd7df
surajkothawade <suraj12378@gmail.com>
2022-07-16 22:20:08 -0500

    Update utils

---
+++ talisman/utils/utils.py
@@ -0,0 +1,32 @@
+import subprocess
+import copy
+
+#---------------------------------------------------------------------------#
+#-------- Custom function to Print Subprocess Output to Console ------------#
+#---------------------------------------------------------------------------#
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
+def _get_config_module(fname):
+    """Load a configuration as a python module."""
+    from mmcv import Config
+    config_mod = Config.fromfile(fname)
+    return config_mod
+
+
+def _get_detector_cfg(fname):
+    """Grab configs necessary to create a detector.
+
+    These are deep copied to allow for safe modification of parameters without
+    influencing other tests.
+    """
+    config = _get_config_module(fname)
+    model = copy.deepcopy(config.model)
+    return model
